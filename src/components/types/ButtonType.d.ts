4f534372017b1aefa157526678754521376f35ca
cistT <cist.b220.t.chitose@gmail.com>
2022-06-10 06:03:15 +0900

    emotion/reactに対応

--- src/components/types/ButtonType.d.ts
+++ src/components/types/ButtonType.d.ts
@@ -1,10 +1,11 @@
 import { CSSProperties } from "react";
-
+import { SerializedStyles } from "@emotion/react";
 type ButtonType = {
     label?: string;
     onClick?: () => void;
     variant?: "text" | "outlined" | "contained" | undefined;
     style?: CSSProperties;
+    emotion?: SerializedStyles;
 };
 
 export default ButtonType;
