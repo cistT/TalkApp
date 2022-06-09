e4585a89f8af848e242d578a329bb092f84ab1c9
cistT <cist.b220.t.chitose@gmail.com>
2022-06-09 09:20:25 +0900

    cssをemotionで書き換え

--- src/components/ui/card/BasicCard.tsx
+++ src/components/ui/card/BasicCard.tsx
@@ -1,10 +1,13 @@
 import { FC } from "react";
 
+import { css } from "@emotion/css";
+
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+
 import BasicCardType from "../../types/BasicCardType";
 
 const BasicCard: FC<BasicCardType> = ({
@@ -14,25 +17,22 @@ const BasicCard: FC<BasicCardType> = ({
     onClick = () => undefined,
 }) => {
     return (
-        <Card
-            sx={{
-                minWidth: "20vw",
-                maxWidth: "60vw",
-                maxHeight: "50vh",
-                margin: "20px",
-            }}
-        >
-            <CardContent style={{ textAlign: "center" }}>
-                <Typography variant="h4" component="div">
+        <Card className={styles.card}>
+            <CardContent className={styles.cardContent}>
+                <Typography
+                    className={styles.cardTitle}
+                    variant="h4"
+                    component="div"
+                >
                     {title}
                 </Typography>
                 <Typography variant="body2">{explanation}</Typography>
             </CardContent>
             <CardActions>
                 <Button
-                    onClick={onClick}
+                    className={styles.cardButton}
                     variant="outlined"
-                    style={{ height: "60px", width: "100%" }}
+                    onClick={onClick}
                 >
                     {buttonLabel}
                 </Button>
@@ -42,3 +42,22 @@ const BasicCard: FC<BasicCardType> = ({
 };
 
 export default BasicCard;
+
+const styles = {
+    card: css`
+        min-width: 20vw;
+        max-width: 60vw;
+        max-height: 50vh;
+        margin: 20px;
+    `,
+    cardContent: css`
+        text-align: center;
+    `,
+    cardTitle: css`
+        font-family: "Kosugi Maru", sans-serif;
+    `,
+    cardButton: css`
+        height: 60px;
+        width: 100%;
+    `,
+};
