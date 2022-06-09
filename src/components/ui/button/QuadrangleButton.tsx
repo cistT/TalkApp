68bdff7b08667204e972c3cc3c8a399ccc635ae0
cistT <cist.b220.t.chitose@gmail.com>
2022-06-10 06:02:34 +0900

    emotion/reactに対応

--- src/components/ui/button/QuadrangleButton.tsx
+++ src/components/ui/button/QuadrangleButton.tsx
@@ -1,25 +1,35 @@
 import { FC } from "react";
 
-import { Button, styled } from "@material-ui/core";
+import { css } from "@emotion/react";
+
+import { Button } from "@material-ui/core";
 
 import ButtonType from "../../types/ButtonType";
-
-const MyStyleButton = styled(Button)({
-    height: "60px",
-    width: "60px",
-});
 
 const QuadrangleButton: FC<ButtonType> = ({
     label = "",
     onClick = () => undefined,
     variant = "outlined",
     style,
+    emotion,
 }) => {
     return (
-        <MyStyleButton style={style} variant={variant} onClick={onClick}>
+        <Button
+            style={style}
+            variant={variant}
+            onClick={onClick}
+            css={emotion ?? styles.button}
+        >
             {label}
-        </MyStyleButton>
+        </Button>
     );
+};
+
+const styles = {
+    button: css`
+        height: 60px;
+        width: 60px;
+    `,
 };
 
 export default QuadrangleButton;
