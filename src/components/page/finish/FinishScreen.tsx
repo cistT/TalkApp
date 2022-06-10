1b52593b846215ca1342c8d2c51ef2b02b4857ae
cistT <cist.b220.t.chitose@gmail.com>
2022-06-10 19:39:33 +0900

    emotionでcssを記述

--- src/components/page/finish/FinishScreen.tsx
+++ src/components/page/finish/FinishScreen.tsx
@@ -1,18 +1,9 @@
-import { CSSProperties, FC } from "react";
+import { FC } from "react";
+import { css } from "@emotion/react";
 
-import { ButtonGroup, styled, Typography } from "@material-ui/core";
+import { ButtonGroup, Typography } from "@material-ui/core";
 
 import CircleButton from "../../ui/button/CircleButton";
-
-const MystyleButtonGroup = styled(ButtonGroup)({
-    display: "flex",
-    justifyContent: "space-around",
-});
-
-const circleButtonStyle: CSSProperties = {
-    height: "130px",
-    width: "130px",
-};
 
 type Props = {
     oneMoreButtonClick: () => void;
@@ -22,47 +13,54 @@ type Props = {
 const FinishScreen: FC<Props> = ({ oneMoreButtonClick, finishButtonClick }) => {
     return (
         <>
-            <Typography
-                variant="h3"
-                component="h4"
-                style={{
-                    height: "20vh",
-                    width: "90vw",
-                    textAlign: "center",
-                    margin: "30px auto",
-                    fontSize: "10vw",
-                }}
-            >
+            <Typography css={styles.title} variant="h3" component="h4">
                 お疲れさまでした
             </Typography>
-            <Typography
-                variant="h5"
-                component="h5"
-                style={{
-                    height: "10vh",
-                    width: "90%",
-                    textAlign: "center",
-                    margin: "10px auto",
-                }}
-            >
+            <Typography css={styles.explanation} variant="h5" component="h5">
                 次に何をしますか？
             </Typography>
 
-            <MystyleButtonGroup>
+            <ButtonGroup css={styles.buttonGroup}>
                 <CircleButton
                     label="メイン画面へ"
+                    emotion={styles.circleButton}
                     onClick={finishButtonClick}
-                    style={circleButtonStyle}
                 />
 
                 <CircleButton
                     label="もう一度"
+                    emotion={styles.circleButton}
                     onClick={oneMoreButtonClick}
-                    style={circleButtonStyle}
                 />
-            </MystyleButtonGroup>
+            </ButtonGroup>
         </>
     );
+};
+
+const styles = {
+    title: css`
+        height: 20vh;
+        width: 90vw;
+        text-align: center;
+        margin: 30px auto;
+        font-size: 10vw;
+        font-family: "Kosugi Maru", sans-serif;
+    `,
+    explanation: css`
+        height: 10vh;
+        width: 90%;
+        text-align: center;
+        margin: 10px auto;
+        font-family: "Kosugi Maru", sans-serif;
+    `,
+    buttonGroup: css`
+        display: flex;
+        justify-content: space-around;
+    `,
+    circleButton: css`
+        height: 130px;
+        width: 130px;
+    `,
 };
 
 export default FinishScreen;
