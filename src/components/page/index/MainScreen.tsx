fdadc7bacea4246dad2a68aef27c8e7843155008
cistT <cist.b220.t.chitose@gmail.com>
2022-06-21 21:35:11 +0900

    アニメーションを追加

--- src/components/page/index/MainScreen.tsx
+++ src/components/page/index/MainScreen.tsx
@@ -20,6 +20,7 @@ type Props = {
 const MainScreen: FC<Props> = ({ cards }) => {
     const mainCard = cards[0];
     const subCards = cards.slice(1);
+
     return (
         <>
             <Typography css={styles.title} variant="h2">
@@ -34,18 +35,19 @@ const MainScreen: FC<Props> = ({ cards }) => {
                     title={mainCard.title}
                     explanation={mainCard.explanation ?? ""}
                     buttonLabel={mainCard.buttonLabel}
-                    emotion={styles.mainCard}
+                    style={styles.mainCard}
                     onClick={mainCard.onClick}
                 />
             </div>
 
-            <div css={styles.cardGroup}>
+            <div css={styles.subCardGroup}>
                 {subCards.map((card, i) => (
                     <BasicCard
                         key={`${card.title + i}`}
                         title={card.title}
                         explanation={card.explanation ?? ""}
                         buttonLabel={card.buttonLabel}
+                        style={styles.subCard}
                         onClick={card.onClick}
                     />
                 ))}
@@ -60,19 +62,78 @@ const styles = {
     title: css`
         text-align: center;
         font-family: "Joan", serif;
-    `,
-    mainCard: css`
-        width: 100vw;
-        max-width: 100vw;
+        animation-name: animation;
+        animation-duration: 0.25s;
+        animation-timing-function: ease;
+        @keyframes animation {
+            0% {
+                transform: translateY(30px);
+            }
+            100% {
+                transform: translateY(0px);
+            }
+        }
     `,
     explanation: css`
         text-align: center;
         margin-top: 5px;
         font-family: "Kosugi Maru", sans-serif;
+        animation-name: animation;
+        animation-duration: 0.25s;
+        animation-timing-function: ease;
+        @keyframes animation {
+            0% {
+                transform: translateY(30px);
+            }
+            100% {
+                transform: translateY(0px);
+            }
+        }
+    `,
+    mainCard: css`
+        width: 100vw;
+        max-width: 100vw;
+        transition: all 0.5s;
+        :hover {
+            transform: scale(1.2);
+        }
     `,
     cardGroup: css`
         display: flex;
         justify-content: center;
         flex-wrap: wrap;
+        animation-name: animation;
+        animation-duration: 0.5s;
+        animation-timing-function: ease;
+        @keyframes animation {
+            0% {
+                transform: translateY(150px);
+            }
+            100% {
+                transform: translateY(0px);
+            }
+        }
+    `,
+    subCardGroup: css`
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        animation-name: animation;
+        animation-duration: 0.75s;
+        animation-timing-function: ease;
+        @keyframes animation {
+            0% {
+                transform: translateY(150px);
+            }
+            100% {
+                transform: translateY(0px);
+            }
+        }
+    `,
+    subCard: css`
+        transition: all 0.5s;
+        :hover {
+            transform: scale(1.2);
+        }
     `,
 };
