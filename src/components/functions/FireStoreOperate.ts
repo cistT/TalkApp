c3712386b2b23f8bf8d7947379c8e821822ea024
cistT <cist.b220.t.chitose@gmail.com>
2022-06-29 19:25:59 +0900

    Cloud Firestoreの記述をV9のものに更新

--- src/components/functions/FireStoreOperate.ts
+++ src/components/functions/FireStoreOperate.ts
@@ -1,32 +1,32 @@
 import { db } from "../../firebase";
-import QuestionStore from "../types/QuestionStoreType";
+import { deleteDoc, doc, setDoc } from "firebase/firestore";
 
-const setFireStore = (collection: string, doc: string, setData: object) => {
-    db.collection(collection).doc(doc).set(setData);
+const setFireStore = async (path: string, pathDoc: string, setData: object) => {
+    await setDoc(doc(db, path, pathDoc), setData);
 };
 
-const deleteFireStore = (collection: string, doc: string) => {
-    db.collection(collection).doc(doc).delete();
+const deleteFireStore = async (path: string, pathDoc: string) => {
+    await deleteDoc(doc(db, path, pathDoc));
 };
 
-const fetchFolder = (uid: string | null | undefined) => {
-    let fetchFolders: QuestionStore = [];
+// const fetchFolder = (uid: string | null | undefined) => {
+//     let fetchFolders: QuestionStore = [];
 
-    db.collection("folder")
-        .where("uid", "==", uid)
-        .onSnapshot((snapShot) => {
-            fetchFolders = [
-                ...snapShot.docs.map((doc) => ({
-                    id: doc.data().folderId,
-                    name: doc.data().name,
-                    questions: [],
-                })),
-            ];
-            console.log(fetchFolders);
-            //ここまではデータがある
-        });
-    console.log(fetchFolders);
-    return fetchFolders;
-};
+//     db.collection("folder")
+//         .where("uid", "==", uid)
+//         .onSnapshot((snapShot) => {
+//             fetchFolders = [
+//                 ...snapShot.docs.map((doc) => ({
+//                     id: doc.data().folderId,
+//                     name: doc.data().name,
+//                     questions: [],
+//                 })),
+//             ];
+//             console.log(fetchFolders);
+//             //ここまではデータがある
+//         });
+//     console.log(fetchFolders);
+//     return fetchFolders;
+// };
 
-export { setFireStore, deleteFireStore, fetchFolder };
+export { setFireStore, deleteFireStore };
